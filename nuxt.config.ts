3919e5e57c272f3abc0b49c09256fe4494a9a809
jtsaich <jtsai0606@gmail.com>
2023-11-04 02:31:35 +0800

    google map with drawing tool

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  plugins: ['~/plugins/vue3-google-map.js'],
-
   build: {
     transpile: ['@fawmi/vue-google-maps'],
   },
+  vite: {
+    optimizeDeps: {
+      include: [
+        "@fawmi/vue-google-maps",
+        "fast-deep-equal"
+      ]
+    }
+  }
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,88 @@
 <template>
-  <div>
-    <GMapMap
-      :center="center"
-      :zoom="7"
-      style="width: 100vw; height: 900px"
-    >
-    </GMapMap>
-  </div>
+  <GMapMap
+    :center="center"
+    ref="myMapRef"
+    :zoom="10"
+    map-type-id="terrain"
+    style="width: 100vw; height: 20rem"
+  >
+    <GMapCluster :zoomOnClick="true">
+      <GMapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center = m.position"
+      />
+    </GMapCluster>
+  </GMapMap>
 </template>
 
 <script>
 export default {
+  mounted() {
+    this.$refs.myMapRef.$mapPromise.then((mapObject) => {
+      console.log(google.maps.drawing.DrawingManager);
+      const drawingManager = new google.maps.drawing.DrawingManager({
+        drawingMode: google.maps.drawing.OverlayType.MARKER,
+        drawingControl: true,
+        drawingControlOptions: {
+          position: google.maps.ControlPosition.TOP_CENTER,
+          drawingModes: [
+            google.maps.drawing.OverlayType.MARKER,
+            google.maps.drawing.OverlayType.CIRCLE,
+            google.maps.drawing.OverlayType.POLYGON,
+            google.maps.drawing.OverlayType.POLYLINE,
+            google.maps.drawing.OverlayType.RECTANGLE,
+          ],
+        },
+        markerOptions: {
+          icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+        },
+        circleOptions: {
+          fillColor: "#ffff00",
+          fillOpacity: 1,
+          strokeWeight: 5,
+          clickable: false,
+          editable: true,
+          zIndex: 1,
+        },
+      });
+
+      drawingManager.setMap(mapObject);
+    });
+  },
   data() {
     return {
-      center: {lat: 51.093048, lng: 6.842120},
-    }
+      center: { lat: 51.093048, lng: 6.84212 },
+      markers: [
+        {
+          position: {
+            lat: 51.093048,
+            lng: 6.84212,
+          },
+        },
+        {
+          position: {
+            lat: 51.198429,
+            lng: 6.69529,
+          },
+        },
+        {
+          position: {
+            lat: 51.165218,
+            lng: 7.067116,
+          },
+        },
+        {
+          position: {
+            lat: 51.09256,
+            lng: 6.84074,
+          },
+        },
+      ],
+    };
   },
 };
 </script>
